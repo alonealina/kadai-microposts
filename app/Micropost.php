d1208081ddf90830a441f6f364dfe3c72ee38180
ryuzo.takagi <william_billl2008@yahoo.co.jp>
2019-07-05 09:42:34 +0000

    kadai-microposts

--- app/Micropost.php
+++ app/Micropost.php
@@ -12,4 +12,12 @@ class Micropost extends Model
     {
         return $this->belongsTo(User::class);
     }
+    
+    //お気に入り機能
+    public function favorite_users()
+    {
+        return $this->belongsToMany(User::class, 'favorites', 'user_id', 'micropost_id')->withTimestamps();
+    }
+
+
 }
